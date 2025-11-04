0a9fdc0bd2aa21e8e3b8c421f7f72efb2f35bc87
stanley-yin <stanley6140992@gmail.com>
2025-11-04 15:56:13 +0800

    feat: add google login feature

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,55 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native"
+import { useFonts } from "expo-font"
+import { Stack } from "expo-router"
+import { StatusBar } from "expo-status-bar"
+import "react-native-reanimated"
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { SplashScreenController } from "@/components/splash-screen-controller"
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { useAuthContext } from "@/hooks/use-auth-context"
+import { useColorScheme } from "@/hooks/use-color-scheme"
+import AuthProvider from "@/providers/auth-provider"
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
+// Separate RootNavigator so we can access the AuthContext
+function RootNavigator() {
+  const { isLoggedIn } = useAuthContext()
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
+    <Stack>
+      <Stack.Protected guard={isLoggedIn}>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
+      </Stack.Protected>
+      <Stack.Protected guard={!isLoggedIn}>
+        <Stack.Screen name="login" options={{ headerShown: false }} />
+      </Stack.Protected>
+      <Stack.Screen name="+not-found" />
+    </Stack>
+  )
+}
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme()
+
+  // const [loaded] = useFonts({
+  //   SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  // })
+  //
+  // if (!loaded) {
+  //   // Async font loading only occurs in development.
+  //   return null
+  // }
+
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <AuthProvider>
+        <SplashScreenController />
+        <RootNavigator />
+        <StatusBar style="auto" />
+      </AuthProvider>
     </ThemeProvider>
-  );
+  )
 }
