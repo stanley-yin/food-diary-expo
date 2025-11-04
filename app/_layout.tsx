f47270817449694ddc5f678000dc2f9020391662
stanley-yin <stanley6140992@gmail.com>
2025-11-04 16:10:39 +0800

    feat: add tailwind.css

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,6 @@ import {
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native"
-import { useFonts } from "expo-font"
 import { Stack } from "expo-router"
 import { StatusBar } from "expo-status-bar"
 import "react-native-reanimated"
@@ -33,15 +32,6 @@ function RootNavigator() {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme()
-
-  // const [loaded] = useFonts({
-  //   SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  // })
-  //
-  // if (!loaded) {
-  //   // Async font loading only occurs in development.
-  //   return null
-  // }
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
