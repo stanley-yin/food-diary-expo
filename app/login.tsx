67a6e13db6840baf68b2df30d041b81c47747883
stanley-yin <stanley6140992@gmail.com>
2025-11-12 21:28:59 +0800

    fix: google auth on mobile

--- app/login.tsx
+++ app/login.tsx
@@ -1,6 +1,6 @@
 import { Link, Stack } from "expo-router"
 import { StyleSheet } from "react-native"
-import GoogleSignInButton from "@/components/social-auth-buttons/google/google-sign-in-button.web"
+import GoogleSignInButton from "@/components/social-auth-buttons/google/google-sign-in-button"
 
 import { ThemedText } from "@/components/themed-text"
 import { ThemedView } from "@/components/themed-view"
